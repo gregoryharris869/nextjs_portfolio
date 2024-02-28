4900adcad5e20d297945521b5422d08ff7018300
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-28 11:48:04 -0600

    1:49

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,29 +1,88 @@
 "use client";
 
-import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
 import React from "react";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[55rem] sm:mb-0 text-justify">
       <div className="flex items-center justify-center">
         <div className="relative">
-          <svg
+          <motion.svg
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
             />
-          </svg>
-          <span className="text-4xl">🧑‍💻</span>
+          </motion.svg>
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0 "
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            🧑‍💻
+          </motion.span>
         </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Gregory Harris.</span> A aspiring{" "}
+        <span className="font-bold">front-end developer.</span> I enjoy building{" "}
+        <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+      <div className="flex flex-col sm:flex-row items-center justify-between  px-4">
+        <Link
+          href="#contact"
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-6 rounded-full"
+        >
+          Contact Me <BsArrowRight />
+        </Link>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+        <a
+          href=""
+          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full"
+        >
+          Download CV <HiDownload />
+        </a>
       </div>
     </section>
   );
