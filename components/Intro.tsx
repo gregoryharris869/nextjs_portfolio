891716aebb090f6722b20b457539082267ced425
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-28 01:43:25 -0600

    1:27

---
+++ components/Intro.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+            />
+          </svg>
+          <span className="text-4xl">🧑‍💻</span>
+        </div>
+      </div>
+    </section>
+  );
+}
