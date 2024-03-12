53f32f61f0ea88780b75211d24562e4dc23b419b
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-03-12 17:15:05 -0500

    6:46

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -39,7 +39,7 @@ export default function Skills() {
               once: true,
             }}
             custom={index}
-            className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
+            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
             key={index}
           >
             {skills}
